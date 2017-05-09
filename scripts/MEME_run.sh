cf7d36af463c56ec0b6afb03bbb81857b1a4395d
Jonathan Rubin <jonathanrubin@rl1-194-116-dhcp.int.colorado.edu>
2017-05-09 10:20:25 -0600

    updates

--- scripts/MEME_run.sh
+++ scripts/MEME_run.sh
@@ -22,5 +22,5 @@ module load meme_4.10.1_4
 
 ### now call your program
 
-/opt/meme/4.10.1_4/bin/meme $fastafile -oc $output -dna
+/opt/meme/4.10.1_4/bin/meme $fastafile -oc $output -maxsize 10000000 -dna
 
