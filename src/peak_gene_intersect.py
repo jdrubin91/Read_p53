f449bb5f92a722bb5cfd65e53f27e84f60d1890c
Jonathan Rubin <jonathanrubin@rl1-194-116-dhcp.int.colorado.edu>
2017-05-11 17:38:00 -0600

    updates

--- src/peak_gene_intersect.py
+++ src/peak_gene_intersect.py
@@ -10,19 +10,26 @@ def parent_dir(directory):
     
     return newdir
 
-def run(DMSO,Nutlin1,Nutlin3,genes):
+def run(DMSO,Nutlin1,Nutlin3,genes,file2dir):
     D = BedTool(DMSO)
     N1 = BedTool(Nutlin1)
     N3 = BedTool(Nutlin3)
-    g = BedTool(genes)
+    g = BedTool(genes).sort().merge(c=5,o="distinct")
 
     w1 = D
     w2 = N1-D
     w3 = N3-N1-D
 
-    print [m[4] for m in g.intersect(w1,u=True)]
-    print [m[4] for m in g.intersect(w2,u=True)]
-    print [m[4] for m in g.intersect(w3,u=True)]
+    outfile1 = open(file2dir + 'wave1_gene_targets.txt','w')
+    outfile2 = open(file2dir + 'wave2_gene_targets.txt','w')
+    outfile3 = open(file2dir + 'wave3_gene_targets.txt','w')
+
+    for item in [str(m[3]) for m in g.intersect(w1,u=True)]:
+        outfile1.write(item + '\n')
+    for item in [str(m[3]) for m in g.intersect(w2,u=True)]:
+        outfile2.write(item + '\n')
+    for item in [str(m[3]) for m in g.intersect(w3,u=True)]:
+        outfile3.write(item + '\n')
 
 
 if __name__ == "__main__":
@@ -39,4 +46,4 @@ if __name__ == "__main__":
     Nutlin3 = file2dir + 'Nutlin3Hr_peaks.merge_200.bed'
     genes = file2dir + 'refGene_hg19_TSS.bed'
 
-    run(DMSO,Nutlin1,Nutlin3,genes)
+    run(DMSO,Nutlin1,Nutlin3,genes,file2dir)
