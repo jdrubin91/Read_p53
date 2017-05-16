01ffc6b7a2fc11be155355a0111fb41a0e7c1cc6
Jonathan Rubin <jonathanrubin@rl1-194-116-dhcp.int.colorado.edu>
2017-05-16 13:53:41 -0600

    updates

--- src/cluster_analysis.py
+++ src/cluster_analysis.py
@@ -9,12 +9,26 @@ from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 import os
 from numpy import random as rn
+import load
 
 def parent_dir(directory):
     pathlist = directory.split('/')
     newdir = '/'.join(pathlist[0:len(pathlist)-1])
     
     return newdir
+
+def find_nearest(array):
+    l = len(array)
+    result = list()
+    for i in range(l):
+        if i == 0:
+            result.append(math.fabs(array[1][0] - array[i][1]))
+        elif i == l-1:
+            result.append(math.fabs(array[i][0] - array[i-1][0]))
+        else:
+            result.append(min(math.fabs(array[i][1] - array[i+1][0]),math.fabs(array[i][0] - array[i-1][1])))
+
+    return result
 
 def run(DMSO,Nutlin1,Nutlin3,P53,figuredir):
     D = BedTool(DMSO)
@@ -40,34 +54,78 @@ def run(DMSO,Nutlin1,Nutlin3,P53,figuredir):
     ax1.set_title('Wave2 to Wave1')
     ax1.set_ylabel('Count')
     ax1.set_xlabel('Distance (bp)')
-    # ax.set_ylim([-2,12])
+    ax1.set_xlim([0,35000000])
     ax1.hist([float(x[-1]) for x in a],bins=100)
 
     ax2 = F.add_subplot(222)
     ax2.set_title('Wave2rand to Wave1rand')
     ax2.set_ylabel('Count')
     ax2.set_xlabel('Distance (bp)')
-    # ax.set_ylim([-2,12])
+    ax2.set_xlim([0,35000000])
     ax2.hist([float(x[-1]) for x in b],bins=100)
 
     ax3 = F.add_subplot(223)
     ax3.set_title('Wave3 to Wave2')
     ax3.set_ylabel('Count')
     ax3.set_xlabel('Distance (bp)')
-    # ax.set_ylim([-2,12])
+    ax3.set_xlim([0,35000000])
     ax3.hist([float(x[-1]) for x in c],bins=100)
 
     ax4 = F.add_subplot(224)
     ax4.set_title('Wave3rand to Wave2rand')
     ax4.set_ylabel('Count')
     ax4.set_xlabel('Distance (bp)')
-    # ax.set_ylim([-2,12])
+    ax4.set_xlim([0,35000000])
     ax4.hist([float(x[-1]) for x in d],bins=100)
 
 
 
     plt.savefig(figuredir + 'Cluster_analysis.png', dpi=1200)
 
+def nearest_neighbor(DMSO,Nutlin1,Nutlin3,P53):
+    a = load.load_bed_points(DMSO)
+    b = load.load_bed_points(Nutlin1)
+    c = load.load_bed_points(Nutlin3)
+    P = load.load_bed_points(P53)
+
+    arand = set()
+    while len(arand) < len(a):
+        arand.add(rn.randint(0,len(P)))
+    arand = list(arand)
+
+    brand = set()
+    while len(arand) < len(b):
+        brand.add(rn.randint(0,len(P)))
+    brand = list(brand)
+
+    crand = set()
+    while len(arand) < len(c):
+        crand.add(rn.randint(0,len(P)))
+    crand = list(crand)
+
+    print len(arand),len(brand),len(crand)
+
+    # F = plt.figure()
+    # ax1 = F.add_subplot(221)
+    # plt.hist([l for l in find_nearest(a)],bins=100)
+    # # ax1.set_xlim([0,500])
+    # ax1.set_title('DMSO')
+    # ax1.set_ylabel('Count')
+    # ax1.set_xlabel('Peak Distance')
+    # ax2 = F.add_subplot(222)
+    # plt.hist([m for m in find_nearest(b)],bins=100)
+    # # ax2.set_xlim([0,500])
+    # ax2.set_title('Nutlin 1hr')
+    # ax2.set_ylabel('Count')
+    # ax2.set_xlabel('Peak Distance')
+    # ax3 = F.add_subplot(223)
+    # plt.hist([n for n in find_nearest(c)],bins=100)
+    # # ax3.set_xlim([0,500]) 
+    # ax3.set_title('Nutlin 3hr')
+    # ax3.set_ylabel('Count')
+    # ax3.set_xlabel('Peak Distance')
+    # # plt.savefig(figuredir + 'nearest_neighbor.png', dpi=1200)
+    # plt.show()
     
 
 
@@ -84,5 +142,8 @@ if __name__ == "__main__":
     Nutlin1 = file2dir + 'Nutlin1Hr_peaks.merge_200.bed'
     Nutlin3 = file2dir + 'Nutlin3Hr_peaks.merge_200.bed'
     P53 = file2dir + 'HO_P53_HUMAN.H10MO.B.pval-6.bed'
+    P53 = '/Users/jonathanrubin/Google Drive/Colorado University/Jonathan/P53_fimo.bed'
+    # run(DMSO,Nutlin1,Nutlin3,P53,figuredir)
 
-    run(DMSO,Nutlin1,Nutlin3,P53,figuredir)
+    P53 = '/Users/jonathanrubin/Google Drive/Colorado University/Jonathan/P53_fimo.no_header.bed'
+    nearest_neighbor(DMSO,Nutlin1,Nutlin3,P53)
