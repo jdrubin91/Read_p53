db33985f77ffe76a6f2e98ec4982baf21202c859
Jonathan Rubin <jonathanrubin@rl1-194-232-dhcp.int.colorado.edu>
2017-06-08 15:12:01 -0600

    updates

--- src/venn_generator.py
+++ src/venn_generator.py
@@ -55,24 +55,26 @@ def promoter_overlap(promoters, DMSO, Nutlin1, Nutlin3, figuredir):
     aB = 0
     Ab = len(D) - AB
     plt.figure()
-    v = venn2(subsets(1,2,3), set_labels = ('DMSO','Promoters'))
+    v = venn2(subsets(Ab,aB,AB), set_labels = ('DMSO','Promoters'))
+    plt.title("Promoter Overlap")
+    plt.savefig(figuredir + 'DMSO_promoter_overlap.png',dpi=1200)
 
 
 def example():
     plt.figure(figsize=(4,4))
-    v = venn3(subsets=(1, 1, 1, 1, 1, 1, 1), set_labels = ('A', 'B', 'C'))
-    print v
-    v.get_patch_by_id('100').set_alpha(1.0)
-    v.get_patch_by_id('100').set_color('white')
-    v.get_label_by_id('100').set_text('Unknown')
-    v.get_label_by_id('A').set_text('Set "A"')
-    c = venn3_circles(subsets=(1, 1, 1, 1, 1, 1, 1), linestyle='dashed')
-    c[0].set_lw(1.0)
-    c[0].set_ls('dotted')
-    plt.title("Sample Venn diagram")
-    plt.annotate('Unknown set', xy=v.get_label_by_id('100').get_position() - np.array([0, 0.05]), xytext=(-70,-70),
-                 ha='center', textcoords='offset points', bbox=dict(boxstyle='round,pad=0.5', fc='gray', alpha=0.1),
-                 arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0.5',color='gray'))
+    # v = venn3(subsets=(1, 1, 1, 1, 1, 1, 1), set_labels = ('A', 'B', 'C'))
+    v = venn2(subsets = (1,2,3), set_labels = ('A','B'))
+    # v.get_patch_by_id('100').set_alpha(1.0)
+    # v.get_patch_by_id('100').set_color('white')
+    # v.get_label_by_id('100').set_text('Unknown')
+    # v.get_label_by_id('A').set_text('Set "A"')
+    # c = venn3_circles(subsets=(1, 1, 1, 1, 1, 1, 1), linestyle='dashed')
+    # c[0].set_lw(1.0)
+    # c[0].set_ls('dotted')
+    # plt.title("Sample Venn diagram")
+    # plt.annotate('Unknown set', xy=v.get_label_by_id('100').get_position() - np.array([0, 0.05]), xytext=(-70,-70),
+                 # ha='center', textcoords='offset points', bbox=dict(boxstyle='round,pad=0.5', fc='gray', alpha=0.1),
+                 # arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0.5',color='gray'))
     plt.show()
 
 if __name__ == "__main__":
@@ -92,4 +94,6 @@ if __name__ == "__main__":
     promoters = file2dir + 'refGene_hg19_TSS.bed'
 
     promoter_overlap(promoters, DMSO, Nutlin1, Nutlin3, figuredir)
+
+    # example()
 
